b4b0878245b1456c26b5ff426feb40b9d1d5e588
Abd0uk <93449982+Abd0uk@users.noreply.github.com>
2026-02-27 07:51:26 +0200

    Add files via upload

---
+++ apps/users/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class UsersConfig(AppConfig):
+    name = "apps.users"  # Updated path
+    verbose_name = _("Users")
+    default_auto_field = "django.db.models.AutoField"
+
+
